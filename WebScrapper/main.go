32bfb75ed48f746bdedc2f131e3e02014e8f65ba
SahilPatel8826 <164241614+SahilPatel8826@users.noreply.github.com>
2025-12-15 00:22:51 +0530

    All the files inserted

--- WebScrapper/main.go
+++ WebScrapper/main.go
@@ -1,61 +1,94 @@
 package main
 
 import (
+	"fmt"
 	"math/rand"
+	"net/http"
+	"net/url"
+	"strings"
 	"time"
+
+	"github.com/PuerkitoBio/goquery"
 )
 
-userAgents= []string{}
+var userAgents = []string{
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38",
+	"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:56.0) Gecko/20100101 Firefox/56.0",
+	"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_13) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/11.0 Safari/604.1.38",
+}
 
-func randomUserAgent() {
+func randomUserAgent() string {
 	rand.Seed(time.Now().Unix())
 	randNum := rand.Int() % len(userAgents)
 	return userAgents[randNum]
 }
 
 func discoverLinks(response *http.Response, baseURL string) []string {
-	if response !=nil{
-		doc,_ := goquery.NewDocumentFromResponse(response)
-		foundLinks := []string{}
+	if response != nil {
+		doc, _ := goquery.NewDocumentFromResponse(response)
+		foundUrls := []string{}
 
 		if doc != nil {
 			doc.Find("a").Each(func(i int, s *goquery.Selection) {
-				res,_:=s.Attr("href")
+				res, _ := s.Attr("href")
 				foundUrls = append(foundUrls, res)
 			})
-			return foundLinks
+			return foundUrls
 		}
-	}else{ 
+	} else {
 		return []string{}
 
-}}
-func getRequest(targetURL string)(*http.Response, error){
- client := &http.Client{}
+	}
+	return []string{}
+}
+func getRequest(targetURL string) (*http.Response, error) {
+	client := &http.Client{}
 
- req, err := http.NewRequest("GET", targetURL, nil)
- if err != nil {
-	return nil, err
- }else{
-	return res,nil
- }
- req.Header.Set("User-Agent", randomUserAgent())
- res, err := client.Do(req)
- if err != nil {
-	return nil, err
- }else{
-	return res,nil
- }
-} 
-func resolve 
+	req, err := http.NewRequest("GET", targetURL, nil)
+	if err != nil {
+		return nil, err
+	}
+	req.Header.Set("User-Agent", randomUserAgent())
+	res, err := client.Do(req)
+	if err != nil {
+		return nil, err
+	} else {
+		return res, nil
+	}
+}
 
-var tokens= make(chan struct{},5)
+func checkRelative(href string, baseUrl string) string {
+	if strings.HasPrefix(href, "/") {
+		return fmt.Sprintf("%s%s", baseUrl, href)
+
+	} else {
+		return href
+	}
+}
+func resolveRelativeLinks(href string, baseUrl string) (bool, string) {
+	resultHref := checkRelative(href, baseUrl)
+	baseParse, _ := url.Parse(baseUrl)
+	resultParse, _ := url.Parse(resultHref)
+	if baseParse != nil && resultParse != nil {
+		if baseParse.Host == resultParse.Host {
+			return true, resultHref
+		} else {
+			return false, ""
+		}
+	}
+	return false, ""
+}
+
+var tokens = make(chan struct{}, 5)
 
 func Crawl(targetURL string, baseURL string) []string {
-	fmt.Printn(targetURL)
+	fmt.Println(targetURL)
 
-	tokens<-struct{}{}
-	resp, _ = getRequest(targetURL)
-    <-tokens
+	tokens <- struct{}{}
+	resp, _ := getRequest(targetURL)
+	<-tokens
 	links := discoverLinks(resp, baseURL)
 	foundUrls := []string{}
 
@@ -67,7 +100,6 @@ func Crawl(targetURL string, baseURL string) []string {
 			}
 		}
 	}
-	ParseHTML(resp)
 	return foundUrls
 }
 
@@ -82,7 +114,7 @@ func main() {
 
 	seen := make(map[string]bool)
 	for ; n > 0; n-- {
-		list := worklist
+		list := <-worklist
 
 		for _, link := range list {
 			if !seen[link] {
@@ -94,7 +126,7 @@ func main() {
 					if foundLinks != nil {
 						worklist <- foundLinks
 					}
-				}(BadExpr)
+				}(link, baseDomain)
 			}
 		}
 	}
